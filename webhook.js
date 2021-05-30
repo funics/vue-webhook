15747864c43d5b87c04fe3e99cd599a283851941
funics <p19991112@qq.com>
2021-05-30 09:39:17 +0800

    shell脚本

--- webhook.js
+++ webhook.js
@@ -1,11 +1,28 @@
 let http = require('http');
+let crypto = require('crypto');
+let SECRET = '123456';
+function sign(body){
+    return `sha1=`+crypto.createHmac('sha1', SECRET).update().digest('hex');
+}
 let server = http.createServer(function(req,res){
     console.log(req.method, req.url);
     if(req.method =='POST'&& req.url == '/webhook'){
+        let buffers = [];
+        req.on('data', function(buffer){
+            buffers.push(buffer);
+        });
+        req.on('end', function(buffer){
+            let body = Buffer.concat(buffers);
+            let event = req.headers['x-gitHub-event']; // event-push
+            let signature = req.headers['x-hub-signature'];
+            if(signature !== sign(body)){
+                return res.end('Not Allowed');
+            }
+        })
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({}));
+        res.end(JSON.stringify({ok: true}));
     }else{
-        res.end(JSON.stringify({}));
+        res.end("Not Found");
     }
 });
 server.listen(4000,()=>{
