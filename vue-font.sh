04862edfbf6e5729ab807ad7cbdd13e54d550dfe
funics <p19991112@qq.com>
2021-05-30 10:56:23 +0800

    add vue-font.sh

--- vue-font.sh
+++ vue-font.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-WORK_PATH='/usr/projects/vue-back'
+WORK_PATH='/usr/projects/vue-font'
 cd $WORK_PATH
 echo "先清除老代码"
 git reset --hard origin/master
 git clean -f
 echo "拉取最新代码"
 git pull origin master
+echo "编译"
+npm run build
 echo "开始执行构建"
-docker build -t vue-back:1.0 .
+docker build -t vue-font:1.0 .
 echo "停止旧容器并删除旧容器"
-docker stop vue-back-container
-docker rm vue-back-container
+docker stop vue-font-container
+docker rm vue-font-container
 echo "启动新容器"
-docker container run -p 3000:3000 --name vue-back-container -d vue-back:1.0
+docker container run -p 3000:3000 --name vue-font-container -d vue-font:1.0
