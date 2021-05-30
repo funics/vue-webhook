b79d194f57635d5f0e9b8dcd4f5f75a154e3d028
funics <p19991112@qq.com>
2021-05-30 09:45:23 +0800

    [BGF]版本号添加

--- vue-back.sh
+++ vue-back.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 WORK_PATH='/usr/projects/vue-back'
 cd $WORK_PATH
-echo "先清楚老代码"
+echo "先清除老代码"
 git reset --hard origin/master
 git clean -f
 echo "拉取最新代码"
 git pull origin master
 echo "开始执行构建"
-docker build -t vue-back .
+docker build -t vue-back:1.0 .
 echo "停止旧容器并删除旧容器"
 docker stop vue-back-container
 docker rm vue-back-container
